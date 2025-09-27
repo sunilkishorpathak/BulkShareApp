0030d33f42fd88291bf780ba3c970b19a6733a0a
sunilkishorpathak <sunilkishorpathak@gmail.com>
2025-09-27 01:14:40 -0700

    Basic SwiftUI project created and tested

---
+++ BulkShareApp/BulkShareAppTests/BulkShareAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BulkShareAppTests.swift
+//  BulkShareAppTests
+//
+//  Created by Sunil Pathak on 9/27/25.
+//
+
+import Testing
+@testable import BulkShareApp
+
+struct BulkShareAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BulkShareApp/BulkShareAppUITests/BulkShareAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BulkShareAppUITestsLaunchTests.swift
+//  BulkShareAppUITests
+//
+//  Created by Sunil Pathak on 9/27/25.
+//
+
+import XCTest
+
+final class BulkShareAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
